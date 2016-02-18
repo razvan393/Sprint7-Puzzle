50860442e390d444a8d0b48afd56505991c23279
razvan393 <razvanghinea05@gmail.com>
2016-02-18 17:31:59 +0200

    Implement background image

--- src/components/Main.js
+++ src/components/Main.js
@@ -15,20 +15,24 @@ var App = React.createClass({
 var Board = React.createClass({
   getInitialState: function () {
     var size = 3;
+    var boardSize = 300;
+    var tileSize = boardSize/size;
     var board = [];
     for (var i = 0; i < (size * size) - 1; i++) {
-      var x = Math.floor(i % size) * 100;
-      var y = Math.floor(i / size) * 100;
+      var x = Math.floor(i % size) * tileSize;
+      var y = Math.floor(i / size) * tileSize;
       var pos = {x: x, y: y};
       board.push({number: i + 1, pos: pos})
     }
     return {
       emptyPos: {
-        x: (size - 1) * 100,
-        y: (size - 1) * 100
+        x: (size - 1) * tileSize,
+        y: (size - 1) * tileSize
       },
       board: board,
-      size: size
+      size: size,
+      tileSize: tileSize,
+      boardSize: boardSize
     }
   },
 
@@ -45,19 +49,20 @@ var Board = React.createClass({
 
   executeShuffle: function () {
     var size = this.state.size;
+    var tileSize = this.state.tileSize;
     var nextShuffleMove = null;
-    var shuffleMoves = Math.floor(Math.random() * (500 - 100) + 100);
+    var shuffleMoves = Math.floor(Math.random() * (900 - 100) + 100);
 
     for (var i = 0; i < shuffleMoves; i++) {
       var emptyPos = this.state.emptyPos;
 
-      if ((emptyPos.x === (size - 1) * 100) && (emptyPos.y === (size - 1) * 100)) {
+      if ((emptyPos.x === (size - 1) * tileSize) && (emptyPos.y === (size - 1) * tileSize)) {
         nextShuffleMove = Math.floor(Math.random() * 2 + 3) % 4;
       } else if ((emptyPos.x === 0) && (emptyPos.y === 0)) {
         nextShuffleMove = Math.floor(Math.random() * 2 + 1);
-      } else if (emptyPos.x === (size - 1) * 100) {
+      } else if (emptyPos.x === (size - 1) * tileSize) {
         nextShuffleMove = Math.floor(Math.random() * 3 + 2) % 4;
-      } else if (emptyPos.y === (size - 1) * 100) {
+      } else if (emptyPos.y === (size - 1) * tileSize) {
         nextShuffleMove = Math.floor(Math.random() * 3 + 3) % 4;
       } else if (emptyPos.y === 0) {
         nextShuffleMove = Math.floor(Math.random() * 3 + 1) % 4;
@@ -70,16 +75,16 @@ var Board = React.createClass({
       if (nextShuffleMove != null) {
         if (nextShuffleMove === 0) {
           //console.log("sus");
-          this.shuffleIt(emptyPos.x, emptyPos.y - 100);
+          this.shuffleIt(emptyPos.x, emptyPos.y - tileSize);
         } else if (nextShuffleMove === 1) {
           //console.log("dreapta");
-          this.shuffleIt(emptyPos.x + 100, emptyPos.y);
+          this.shuffleIt(emptyPos.x + tileSize, emptyPos.y);
         } else if (nextShuffleMove === 2) {
           //console.log("jos");
-          this.shuffleIt(emptyPos.x, emptyPos.y + 100);
+          this.shuffleIt(emptyPos.x, emptyPos.y + tileSize);
         } else {
           //console.log("stanga");
-          this.shuffleIt(emptyPos.x - 100, emptyPos.y);
+          this.shuffleIt(emptyPos.x - tileSize, emptyPos.y);
         }
       }
     }
@@ -96,20 +101,24 @@ var Board = React.createClass({
   render: function () {
     var size = this.state.size;
     var tiles = [];
+    var style = {width: this.state.boardSize, height: this.state.boardSize};
+
     for (var i = 0; i < (size * size) - 1; i++) {
       var tile = <Tile
         index={i}
         key={i}
         number={i+1}
+        size={size}
+        boardSize={this.state.boardSize}
         position={this.state.board[i].pos}
-        onClick={this.onTileClick}
-      />
+        tileSize={this.state.tileSize}
+        onClick={this.onTileClick}/>;
       tiles.push(tile)
     }
 
     return (
       <div>
-        <div className="board">
+        <div className="board" style={style}>
           {tiles}
         </div>
         <button className="shuffle-btn" onClick={this.onClickShuffle}>
@@ -123,18 +132,35 @@ var Board = React.createClass({
     if (!isValidMove) {
       return alert('No good!Go home!')
     }
-    var obj = {};
-    obj = this.state.board[index].pos;
+    var obj = this.state.board[index].pos;
     this.state.board[index].pos = this.state.emptyPos;
     this.state.emptyPos = obj;
-    this.forceUpdate();
+    this.forceUpdate(function () {
+      if (this.isWin()) {
+        alert('We got big succes');
+        this.executeShuffle();
+      }
+    });
+  },
+
+  isWin: function () {
+    for(var i = 0; i< this.state.board.length; i++) {
+      var x = this.state.board[i].pos.x / this.state.tileSize;
+      var y = this.state.board[i].pos.y / this.state.tileSize;
+      var size = this.state.size;
+      if ((x+ y*size)!==this.state.board[i].number -1) {
+        return false;
+      }
+    }
+    return true;
   },
 
   isValidMove: function (startPos, targetPos) {
+    var tileSize = this.state.tileSize;
     var diffX = Math.abs(targetPos.x - startPos.x);
     var diffY = Math.abs(targetPos.y - startPos.y);
-    var validX = diffX === 100 && diffY === 0;
-    var validY = diffY === 100 && diffX === 0;
+    var validX = diffX === tileSize && diffY === 0;
+    var validY = diffY === tileSize && diffX === 0;
 
     if (validX || validY) {
       return true;
@@ -142,8 +168,6 @@ var Board = React.createClass({
 
     return false
   }
-
-
 });
 
 var Tile = React.createClass({
@@ -154,8 +178,14 @@ var Tile = React.createClass({
 
   render: function () {
     var pos = this.props.position;
-    var style = {left: pos.x, top: pos.y};
     var number = this.props.number;
+    var index = this.props.index;
+    var size = this.props.size;
+    var tileSize = this.props.tileSize;
+    var boardSize = this.props.boardSize;
+    var x = -tileSize*index%boardSize;
+    var y = -(tileSize*Math.floor(index/size));
+    var style = {left: pos.x, top: pos.y, width: tileSize, height: tileSize, backgroundPositionX: x, backgroundPositionY: y};
     return (
       <div className="tile" style={style} onClick={this.onClick}>
         <span>{ number }</span>
